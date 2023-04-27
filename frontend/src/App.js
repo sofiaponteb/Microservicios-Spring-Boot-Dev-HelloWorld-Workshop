51e06028971f5e005b7f8fe3fc72b76553d0d61d
sofiaponteb <aaponte@unal.edu.co>
2023-04-27 14:34:25 -0500

    archivos workshop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,8 @@ import React, { useState } from 'react'
 import ChatList from './components/ChatList'
 import ChatForm from './components/ChatForm'
 import Header from './components/Header'
-import ChatAdios from './components/ChatAdios'
 import Card from './components/shared/Card'
 import BtnSubmit from './components/BtnSubmit'
-import BtnResponse from './components/BtnResponse'
 import {ChatProvider} from "./context/ChatContext"
 
 function App() {
@@ -13,20 +11,6 @@ function App() {
 
   return (
     <ChatProvider>
-    <Header/>
-    <div className='container'>
-      <Card>
-        <ChatForm />
-        <ChatAdios />
-        <div className='btn-send'> 
-          <BtnSubmit />
-          <BtnResponse />
-        </div>
-      </Card>
-
-      <ChatList chat = {chat} />
-      
-    </div>
     </ChatProvider>
   )
 }
--- frontend/src/context/ChatContext.js
+++ frontend/src/context/ChatContext.js
@@ -23,33 +23,6 @@ export const ChatProvider = ({children}) => {
         //console.log(text)
     }
 
-    const getHola = async () => {
-        const response = await fetch(`http://localhost:65230/saludar`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            mode: 'no-cors',
-        } )
-        //data = await response.json()
-        const data = 'Holiwi'
-        setTextHola(data)
-        console.log(data)
-    }
-
-    const addAdios = async(text) => {
-        const response = await fetch(`http://localhost:65232/adios?Despedida=${text}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(text),
-            mode: 'no-cors',
-        })
-
-        //setTextAdios(text)
-    }
-
     const addChat = async (newChat) => {
         setChat([newChat, ...chat])
         //console.log(newChat)
@@ -61,9 +34,7 @@ export const ChatProvider = ({children}) => {
             textAdios,
             chat,
             addHola,
-            addAdios,
             addChat,
-            getHola,
         }}>
             {children}
         </ChatContext.Provider>
